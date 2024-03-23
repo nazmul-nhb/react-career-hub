2988609f1cbc06cfc1bef9e81ce8ffdfe9431f72
nazmul-nhb <nazmulnhb007@yahoo.com>
2024-03-23 12:47:41 +0600

    designed cards layout

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -19,7 +19,7 @@ const FeaturedJobs = () => {
                 <h2 className="text-5xl">Featured Jobs : {jobs.length}</h2>
                 <p className="">Explore thousands of job opportunities with all the information you need. Its your future</p>
             </div>
-            <div>
+            <div className="grid grid-cols-2 gap-6">
                 {
                     jobs.map(job=><FeaturedJob key={job.id} job={job}></FeaturedJob>)
                 }
